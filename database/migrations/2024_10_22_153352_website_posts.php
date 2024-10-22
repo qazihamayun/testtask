6e13dd703daffe688f5f053848132fa6c9c6a9f0
Qazi Hamayun <hamayun4u2004@gmail.com>
2024-10-22 20:51:31 +0500

    added website posts migration

---
+++ database/migrations/2024_10_22_153352_website_posts.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('website_posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('website_id');
+            $table->string('title');
+            $table->string('title_slug')->nullable();
+            $table->text('description');
+            $table->enum('status',['draft', 'published','unpublished'])->default('draft');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('website_posts');
+    }
+};
