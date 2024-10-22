1801187745e08c0992d819c11b03963b06c2f1ee
Qazi Hamayun <hamayun4u2004@gmail.com>
2024-10-22 21:26:12 +0500

    added relationships in email log model

--- app/Models/EmailLog.php
+++ app/Models/EmailLog.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\WebsitePost;
+use App\Models\User;
 class EmailLog extends Model
 {
     use HasFactory;
@@ -13,4 +14,13 @@ class EmailLog extends Model
     protected $fillable = ['website_post_id','user_id'];
 
 
+    public function websitePost()
+    {
+        return $this->belongsTo(WebsitePost::class, 'website_post_id','id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id','id');
+    }
 }
